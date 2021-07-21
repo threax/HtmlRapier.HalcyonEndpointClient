2e1bef5135a3b6906220f22e24d9979f09190d69
Andrew Piper <andrew@threax.com>
2021-07-21 12:08:55 -0400

    Added tag push.

--- Push.ps1
+++ Push.ps1
@@ -1,4 +1,8 @@
 git push
+git push --tags
 git push https://github.com/threax/HtmlRapier.HalcyonEndpointClient.git
+git push https://github.com/threax/HtmlRapier.HalcyonEndpointClient.git --tags
 git push https://gitlab.com/threax-software/HtmlRapier.HalcyonEndpointClient.git
+git push https://gitlab.com/threax-software/HtmlRapier.HalcyonEndpointClient.git --tags
 git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/HtmlRapier.HalcyonEndpointClient
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/HtmlRapier.HalcyonEndpointClient --tags
